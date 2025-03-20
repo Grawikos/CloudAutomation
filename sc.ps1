4856ec8e609422f03c549a7f65a5b362f0150f1c
Marcin Grawinski <mar.grawinski@gmail.com>
2025-03-20 14:23:06 +0100

    upload every 10 min

--- sc.ps1
+++ sc.ps1
@@ -3,9 +3,12 @@ aws cloudformation wait stack-create-complete --stack-name Networking
 
 aws cloudformation create-stack --stack-name EFS --template-body file://efs.yml
 aws cloudformation create-stack --stack-name SSMS --template-body file://ssms.yml
+aws cloudformation create-stack --stack-name S3Athena --template-body file://bucket.yml --parameters ParameterKey=LabAccountID,ParameterValue=089087866202
+
 
 aws cloudformation wait stack-create-complete --stack-name EFS
 aws cloudformation wait stack-create-complete --stack-name SSMS
+aws cloudformation wait stack-create-complete --stack-name S3Athena
 
 aws cloudformation create-stack --stack-name Instances --template-body file://instances.yml
 aws cloudformation wait stack-create-complete --stack-name Instances
--- sc_delete.ps1
+++ sc_delete.ps1
@@ -1,11 +1,13 @@
 aws cloudformation delete-stack --stack-name Instances
 aws cloudformation wait stack-delete-complete --stack-name Instances
 
-aws cloudformation delete-stack --stack-name SSMS
+aws s3 rm s3://athena-data-bucket-089087866202 --recursive
+aws cloudformation delete-stack --stack-name S3Athena 
+aws cloudformation delete-stack --stack-name EFS 
+aws cloudformation delete-stack --stack-name SSMS 
+
+aws cloudformation wait stack-delete-complete --stack-name S3Athena
 aws cloudformation wait stack-delete-complete --stack-name SSMS
-
-
-aws cloudformation delete-stack --stack-name EFS
 aws cloudformation wait stack-delete-complete --stack-name EFS
 
 aws cloudformation delete-stack --stack-name Networking
