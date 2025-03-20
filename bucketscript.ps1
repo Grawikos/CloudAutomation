9633c28938c84dc0947168ebf93008d8448065d3
Marcin Grawinski <mar.grawinski@gmail.com>
2025-03-20 15:18:46 +0100

    now script shows s3 website address

--- bucketscript.ps1
+++ bucketscript.ps1
@@ -10,3 +10,5 @@ aws s3api put-bucket-ownership-controls --bucket athena-data-bucket-$userid --ow
 aws s3api put-object-acl --bucket athena-data-bucket-$userid --key index.html --acl public-read
 aws s3api put-bucket-policy --bucket athena-data-bucket-$userid --policy file://policy.json
 aws s3 cp index.html s3://athena-data-bucket-$userid/ --acl public-read --content-type "text/html"
+echo Website:
+echo "http://athena-data-bucket-$userid.s3-website-us-east-1.amazonaws.com/"
