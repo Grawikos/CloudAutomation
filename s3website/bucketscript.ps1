75426dd800395a4de992eb7b485a193698ff9175
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-02 17:32:38 +0200

    refined bucket script to always use userid

--- s3website/bucketscript.ps1
+++ s3website/bucketscript.ps1
@@ -9,12 +9,15 @@ if (!$bucketname) {
     $bucketname="athena-data-bucket"
 } 
 
+$policy = '{\"Version\": \"2012-10-17\", \"Statement\": [{\"Sid\": \"PublicReadGetObject\", \"Effect\": \"Allow\", \"Principal\": \"*\", \"Action\": \"s3:GetObject\", \"Resource\": \"arn:aws:s3:::' + $bucketname + '-' + $userid + '/*\"}]}'
+
+echo $policy
 aws s3api put-bucket-website --bucket $bucketname-$userid --website-configuration file://website.json
 aws s3api put-public-access-block --bucket $bucketname-$userid --public-access-block-configuration "BlockPublicAcls=false"
 aws s3api put-bucket-ownership-controls --bucket $bucketname-$userid --ownership-controls="Rules=[{ObjectOwnership=ObjectWriter}]"
-aws s3api put-bucket-policy --bucket $bucketname-$userid --policy file://policy.json
-aws s3 cp s3://athena-data-bucket-089087866202/out.txt s3://athena-data-bucket-089087866202/tmp.txt
-aws s3 rm s3://athena-data-bucket-089087866202/tmp.txt
+aws s3api put-bucket-policy --bucket $bucketname-$userid --policy "$policy"
+aws s3 cp s3://$bucketname-$userid/out.txt s3://$bucketname-$userid/tmp.txt
+aws s3 rm s3://$bucketname-$userid/tmp.txt
 
 echo Website:
 echo "http://$bucketname-$userid.s3-website-us-east-1.amazonaws.com/"
