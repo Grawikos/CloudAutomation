c677c0ab3cd8f83472d805bd71c54b2a650cd482
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-02 12:46:44 +0200

    improved scripts: ssm now with -port, instanceid is automatically inserted, bucketscript now creates index.html at run

--- connect_admins.ps1
+++ connect_admins.ps1
@@ -1,9 +1,10 @@
-param($instanceid)
-if (!$instanceid){
-    echo "expected -instanceid, not found"
-    Break
+param($port)
+if (!$port){
+    $port=8080
 }
+$params = '{\"portNumber\":[\"5601\"], \"localPortNumber\":[\"' + $port + '\"]}'
+echo http://localhost:$port
 
-aws ssm start-session --target $instanceid --document-name AWS-StartPortForwardingSession --parameters '{\"portNumber\":[\"5601\"], \"localPortNumber\":[\"8080\"]}'
-echo "Website:"
-echo "http://localhost:8080"
+$instanceid=$( aws ec2 describe-instances --filters "Name=tag:Name,Values=MonitoringInstance" --query 'Reservations[0].Instances[0].InstanceId' )
+aws ssm start-session --target $instanceid --document-name AWS-StartPortForwardingSession --parameters $params
+
--- s3website/bucketscript.ps1
+++ s3website/bucketscript.ps1
@@ -1,7 +1,8 @@
 param($userid,$bucketname)
 if (!$userid) {
     echo "-userid expected"
-    Exit
+    # -userid "089087866202"
+    Break
 } 
 if (!$bucketname) {
     echo "-bucketname expected, defaulting to athena-data-bucket"
@@ -12,5 +13,8 @@ aws s3api put-bucket-website --bucket $bucketname-$userid --website-configuratio
 aws s3api put-public-access-block --bucket $bucketname-$userid --public-access-block-configuration "BlockPublicAcls=false"
 aws s3api put-bucket-ownership-controls --bucket $bucketname-$userid --ownership-controls="Rules=[{ObjectOwnership=ObjectWriter}]"
 aws s3api put-bucket-policy --bucket $bucketname-$userid --policy file://policy.json
+aws s3 cp s3://athena-data-bucket-089087866202/out.txt s3://athena-data-bucket-089087866202/tmp.txt
+aws s3 rm s3://athena-data-bucket-089087866202/tmp.txt
+
 echo Website:
 echo "http://$bucketname-$userid.s3-website-us-east-1.amazonaws.com/"
