c94e708a8a043edfe1625b82736a3b8eb77df66c
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-03 11:33:53 +0200

    last small visual changes 2 (didnt add all to last commit -\_(;/)_/-)

--- connect_admins.ps1
+++ connect_admins.ps1
@@ -3,6 +3,7 @@ if (!$port){
     $port=8080
 }
 $params = '{\"portNumber\":[\"5601\"], \"localPortNumber\":[\"' + $port + '\"]}'
+echo "Website:"
 echo http://localhost:$port
 
 $instanceid=$( aws ec2 describe-instances --filters "Name=tag:Name,Values=MonitoringInstance" --query 'Reservations[0].Instances[0].InstanceId' )
