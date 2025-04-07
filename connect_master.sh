f1af50ae650aca5020bbeef52a7362a39fa77a1b
Siema Maciek <116840498+OwczarekMaciej@users.noreply.github.com>
2025-04-07 19:45:07 +0200

    added sm script for master

---
+++ connect_master.sh
@@ -0,0 +1,9 @@
+instanceid=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=BuildInstance" \
+  --query 'Reservations[0].Instances[0].InstanceId' \
+  --output text)
+
+aws ssm start-session \
+  --target "$instanceid" \
+  --document-name AWS-StartPortForwardingSession \
+  --parameters '{"portNumber":["5601"], "localPortNumber":["8080"]}'
