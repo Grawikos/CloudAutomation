846017cacbb511e2d51ef7bd1fac0c701f513b37
Siema Maciek <116840498+OwczarekMaciej@users.noreply.github.com>
2025-04-10 01:34:36 +0200

    jakies cos

--- gcp/ansible/generate_inventory.sh
+++ gcp/ansible/generate_inventory.sh
@@ -5,10 +5,10 @@ NODE1_IP=$(terraform output -raw node1_ip)
 
 cat > inventory.ini <<EOF
 [kube_master]
-master ansible_host=$MASTER_IP ansible_user=ubuntu
+master ansible_host=$MASTER_IP ansible_user=ubuntu ansible_ssh_private_key_file=/Users/maciek/.ssh/my_key
 
 [kube_nodes]
-node1 ansible_host=$NODE1_IP ansible_user=ubuntu
+node1 ansible_host=$NODE1_IP ansible_user=ubuntu ansible_ssh_private_key_file=/Users/maciek/.ssh/my_key
 
 [kubernetes:children]
 kube_master
