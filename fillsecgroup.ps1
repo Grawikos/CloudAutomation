4f1127b1890d1f6953bc7d97cbbc3a08795cb1f2
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-10 23:00:04 +0200

    report advances

--- fillsecgroup.ps1
+++ fillsecgroup.ps1
@@ -1,6 +1,6 @@
 param($ip1,$ip2,$ip3)
 
-$SECURITY_GROUP_ID = aws ec2 describe-security-groups --filters Name=group-name,Values=DBSecurityGroup  --query 'SecurityGroups[0].GroupId' --output text
+$SECURITY_GROUP_ID = aws ec2 describe-security-groups --filters "Name=tag:Name,Values=RDSSG"  --query 'SecurityGroups[0].GroupId' --output text
 aws ec2 authorize-security-group-ingress  --group-id "$SECURITY_GROUP_ID" --protocol "tcp" --port "1433" --cidr "$ip1"
 aws ec2 authorize-security-group-ingress  --group-id "$SECURITY_GROUP_ID" --protocol "tcp" --port "1433" --cidr "$ip2"
 aws ec2 authorize-security-group-ingress  --group-id "$SECURITY_GROUP_ID" --protocol "tcp" --port "1433" --cidr "$ip3"
