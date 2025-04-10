99a56f692ff77a3a14f86a660c9646cce5fc6e42
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-10 22:14:08 +0200

    scripts to finish deployment

---
+++ createReverseProxy.ps1
@@ -0,0 +1,8 @@
+param($GCEALB)
+
+aws cloudformation create-stack --stack-name ReverseProxy --template-body file://AWS_CF_Templates/ReverseProxy.yml --parameters ParameterKey=GCEALB,ParameterValue=$GCEALB
+aws cloudformation wait stack-create-complete --stack-name ReverseProxy
+
+
+echo "Link to the website:"
+echo "http://$(aws cloudformation describe-stacks --stack-name ReverseProxy --query 'Stacks[0].Outputs[?OutputKey==`ProxyInstancePublicIP`].OutputValue' --output text)"
