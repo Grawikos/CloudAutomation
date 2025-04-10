72807d00def016b388ab6620dcfe300d394637b2
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-10 17:45:30 +0200

    added reverse proxy, changed node pool to be zonal not regional, red publically available, ecr, s3, now emptied before deletion

--- aws/modules/ECR/main.tf
+++ aws/modules/ECR/main.tf
@@ -3,3 +3,12 @@ resource "aws_cloudformation_stack" "ecr" {
   template_body = file(var.template_path)
 }
 
+resource "null_resource" "pre_destroy_cleanup" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+      aws ecr delete-repository --repository-name appimagerepository --force || true
+    EOT
+  }
+  depends_on = [ aws_cloudformation_stack.ecr ]
+}
