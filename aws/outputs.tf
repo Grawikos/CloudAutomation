1ce81be3491056fdf119353a36a13312e715b3f0
Marcin Grawinski <mar.grawinski@gmail.com>
2025-04-10 21:30:20 +0200

    minor changes

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -2,3 +2,8 @@ output "appautoscaling_lb_dns" {
   description = "DNS name of the load balancer from appautoscaling module"
   value       = module.appautoscaling.load_balancer_dns
 }
+
+output "reverse_proxy_ipv4" {
+  description = "ip of the single address to access both clouds"
+  value = module.reverseproxy.reverseproxy_instance_public_ip 
+}
