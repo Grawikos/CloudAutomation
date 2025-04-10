9804e7d4abda12b40e434708c134dad41af4f8d3
Siema Maciek <116840498+OwczarekMaciej@users.noreply.github.com>
2025-04-10 15:27:15 +0200

    ansible deployment

--- gcp/main.tf
+++ gcp/main.tf
@@ -69,11 +69,11 @@ resource "google_container_node_pool" "primary_nodes" {
   location = google_container_cluster.primary.location
   cluster  = google_container_cluster.primary.name
 
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-medium"
-    disk_size_gb = 40 # default for micro is 100GB, not needed
+    disk_size_gb = 40
     disk_type    = "pd-standard"
 
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
